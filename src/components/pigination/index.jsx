9025ec8df2f0ba4056279166480dd3f389454e6b
oyangor <robertanthonyoyango@gmail.com>
2024-03-21 19:59:03 +0300

    pagination

---
+++ src/components/pigination/index.jsx
@@ -0,0 +1,26 @@
+
+import React from 'react'
+
+function Pagination({ currentPage = 1, totalPages = 10, onPageChange }) {
+
+  function generateNoOfPages() {
+    const pages = []
+    for (let index = 1; index <= totalPages; index++) {
+      pages.push(index)
+    }
+    return pages
+  }
+  return (
+    <div>
+      <button onClick={() => onPageChange(currentPage - 1)} disabled={currentPage === 1}>{currentPage === 1 ? 'disabled' : 'PREV'}</button>
+      {
+        generateNoOfPages().map((pageNo) => (
+          <button key={pageNo} onClick={() => onPageChange(pageNo)}>{pageNo}</button>
+        ))
+      }
+      <button onClick={() => onPageChange(currentPage + 1)} disabled={currentPage === 10} >{currentPage === 10 ? 'disabled' : 'NEXT'}</button>
+    </div>
+  )
+}
+
+export default Pagination
---
+++ src/components/pigination/test.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import Pagination from '.'
+import { useState } from 'react'
+
+function PaginationTest() {
+  const [currentPage, setCurrentPage] = useState(1)
+
+  const dummyData = Array.from({ length: 100 }, (_, index) => ({
+    id: index + 1,
+    name: `Product ${index + 1}`
+  }))
+
+  const itemsPerPage = 10
+
+  function handlePageChange(currentPage) {
+    setCurrentPage(currentPage)
+  }
+
+  const indexOfLastItem = currentPage * itemsPerPage
+  const indexOfFirstItem = indexOfLastItem - itemsPerPage
+  const currentListOfItems = dummyData.slice(indexOfFirstItem, indexOfLastItem)
+
+  console.log(indexOfFirstItem, indexOfLastItem, currentListOfItems);
+
+  return (
+    <div>
+      <h2>Pagination</h2>
+      <ul className='flex mt-10 justify-between items-center mb-10 gap-2 h-5'>
+        {
+          currentListOfItems.map((listItem) => (
+            <li key={listItem.id} className=' w-[50] h-[50] p-[20px] text-white text-center bg-slate-900 border border-solid border-gray-700'>{listItem.name}</li>
+          ))
+        }
+      </ul>
+      <Pagination
+        currentPage={currentPage}
+        totalPages={Math.ceil(dummyData.length / itemsPerPage)}
+        onPageChange={handlePageChange}
+      />
+    </div>
+  )
+}
+
+export default PaginationTest
